ea80c4ed9f22a7b343dcf6744e1a53a6e3bfa59a
yuanji.zhai <zack@yuanjizhaideMacBook-Pro.local>
2021-10-12 16:31:23 +0800

    add test table code.

--- src/views/doctorAdmin/settings/Settings.js
+++ src/views/doctorAdmin/settings/Settings.js
@@ -1,6 +1,38 @@
 
 export default function Settings() {
     return (
-        <div className={'w-full h-screen bg-yellow-500'}></div>
+        <div className={'w-full h-screen bg-yellow-500'}>
+            <div className={'m-4 border-b'}>
+                <h1>Settings</h1>
+            </div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Month</th>
+                            <th>Savings</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <td>Sum</td>
+                            <td>$180</td>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        <tr>
+                            <td>January</td>
+                            <td>$100</td>
+                        </tr>
+                        <tr>
+                            <td>February</td>
+                            <td>$80</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     )
 }
