4c556578a79d5e8e68cea7f1a1a549068706aed8
zhaiyuanji <chuck.zhaiyj@gmail.com>
2021-10-13 16:55:18 +0800

    add monday settings

---
+++ src/views/doctorAdmin/settings/DateTimeListBox.js
@@ -0,0 +1,73 @@
+import {Listbox, Transition} from "@headlessui/react";
+import React, {Fragment, useState} from "react";
+
+const DateTimeListBox = React.memo(({selected, isDisabled, dataSource, onChangeValue}) => {
+    const onChange = (value) => {
+        onChangeValue && onChangeValue(value)
+    }
+
+    const getTitle = (value) => {
+        const item = dataSource.find((_item) => {
+            return _item && _item.value.length && _item.value === value
+        })
+        return item.title
+    }
+
+    const title = getTitle(selected)
+
+    return (
+        <div className="min-w-min z-20">
+            <Listbox disabled={isDisabled} value={selected} onChange={onChange} >
+                <div className="relative mt-0">
+                    <Listbox.Button className={`relative py-1 px-4 cursor-default rounded-full flex flex-row items-center justify-between border border-gray-400  ${isDisabled ? 'bg-gray-200': 'bg-white hover:bg-gray-100'}`}>
+                        <span className={`font-mono text-sm font-semibold text-gray-600 mr-2`}>{title}</span>
+                        <span className="">
+									<i className={`fas fa-chevron-down text-sm text-gray-600`}></i>
+            					</span>
+                    </Listbox.Button>
+                    <Transition
+                        as={Fragment}
+                        enter="transition duration-100 ease-out"
+                        enterFrom="transform scale-95 opacity-0"
+                        enterTo="transform scale-100 opacity-100"
+                        leave="transition duration-75 ease-out"
+                        leaveFrom="transform scale-100 opacity-100"
+                        leaveTo="transform scale-95 opacity-0"
+                    >
+                        <Listbox.Options className="absolute w-48 py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-2xl border max-h-72">
+                            {dataSource.map((gender, idx) => {
+                                return (
+                                    <Listbox.Option
+                                        key={idx}
+                                        value={gender.value}
+                                    >
+                                        {({ selected, active }) => {
+                                            return (
+                                                <div
+                                                    className={`${
+                                                        active ? 'bg-primary text-white font-medium' : 'bg-white text-black font-normal'
+                                                    } w-full py-2 px-4 font-mono text-sm`}
+                                                >
+                                                    {gender.title}
+                                                </div>
+                                            )
+                                        }}
+                                    </Listbox.Option>
+                                )
+                            })}
+                        </Listbox.Options>
+                    </Transition>
+                </div>
+            </Listbox>
+        </div>
+    )
+})
+
+DateTimeListBox.defaultProps = {
+    isDisabled: true,
+    selected: '',
+    dataSource: [],
+    onChangeValue: undefined
+}
+
+export default DateTimeListBox
--- src/views/doctorAdmin/settings/Settings.js
+++ src/views/doctorAdmin/settings/Settings.js
@@ -2,55 +2,96 @@ import React, { useState } from 'react'
 import { Switch } from '@headlessui/react'
 import {FilterActionType} from "../../../hooks/filter/FilterProvider";
 import ListBox from "../../../components/navBar/ListBox";
+import DateTimeListBox from "./DateTimeListBox";
 
 export default function Settings() {
     const [enabled, setEnabled] = useState(false)
+    const [mondayIsAmEnable, setMondayIsAmEnable] = useState(false)
     const dateTimeDateSource = [{title: '08:00', value: "08:00"}, {title: '08:00', value: "08:00"}, {title: '08:00', value: "08:00"}]
     const renderWorkingHourItem = () => {
         return (
             <tr>
-                <td>Monday</td>
-                <td className={'flex flex-row items-center'}>
-                    <Switch
-                        checked={enabled}
-                        onChange={setEnabled}
-                        className={`${
-                            enabled ? 'bg-blue-600' : 'bg-gray-200'
-                        } relative inline-flex items-center h-6 rounded-full w-11 mr-4`}
-                    >
-                        <span className="sr-only">Enable notifications</span>
-                        <span
-                            className={`${
-                                enabled ? 'translate-x-6' : 'translate-x-1'
-                            } inline-block w-4 h-4 transform bg-white rounded-full`}
-                        />
-                    </Switch>
-
-                    <ListBox
-                        dataSource={dateTimeDateSource}
-                        defaultTitle = {''}
-                        selected={'08:00'}
-                        onChangeValue={(value) => {
-
-                        }}
-                    />
-
-                    <p className={'mx-2 font-black'}>{' -- '}</p>
-                    <ListBox
-                        dataSource={dateTimeDateSource}
-                        defaultTitle = {''}
-                        selected={'08:00'}
-                        onChangeValue={(value) => {
-
-                        }}
-                    />
+                <td>
+                    <p className={'mx-4 font-semibold text-black'}>Monday</p>
                 </td>
-                <td>$100</td>
+                <td>
+                    <div className={'flex flex-row items-center'}>
+                        <Switch
+                            checked={mondayIsAmEnable}
+                            onChange={setMondayIsAmEnable}
+                            className={`${
+                                mondayIsAmEnable ? 'bg-primary' : 'bg-gray-200'
+                            } relative inline-flex items-center h-6 rounded-full w-11 mr-4`}
+                        >
+                            <span
+                                className={`${
+                                    mondayIsAmEnable ? 'translate-x-6' : 'translate-x-1'
+                                } inline-block w-4 h-4 transform bg-white rounded-full`}
+                            />
+                        </Switch>
+
+                        <DateTimeListBox
+                            isDisabled={!mondayIsAmEnable}
+                            dataSource={dateTimeDateSource}
+                            selected={'08:00'}
+                            onChangeValue={(value) => {
+
+                            }}
+                        />
+                        <p className={'mx-2 text-sm text-base-black font-semibold'}>{' to '}</p>
+                        <DateTimeListBox
+                            isDisabled={!mondayIsAmEnable}
+                            dataSource={dateTimeDateSource}
+                            selected={'08:00'}
+                            onChangeValue={(value) => {
+
+                            }}
+                        />
+                    </div>
+                </td>
+                <td>
+                    <div className={'w-px h-6 bg-gray-400 mx-5'}/>
+                </td>
+                <td>
+                    <div className={'flex flex-row items-center'}>
+                        <Switch
+                            checked={mondayIsAmEnable}
+                            onChange={setMondayIsAmEnable}
+                            className={`${
+                                mondayIsAmEnable ? 'bg-primary' : 'bg-gray-200'
+                            } relative inline-flex items-center h-6 rounded-full w-11 mr-4`}
+                        >
+                            <span className="sr-only">Enable notifications</span>
+                            <span
+                                className={`${
+                                    mondayIsAmEnable ? 'translate-x-6' : 'translate-x-1'
+                                } inline-block w-4 h-4 transform bg-white rounded-full`}
+                            />
+                        </Switch>
+
+                        <DateTimeListBox
+                            isDisabled={!mondayIsAmEnable}
+                            dataSource={dateTimeDateSource}
+                            selected={'08:00'}
+                            onChangeValue={(value) => {
+
+                            }}
+                        />
+                        <p className={'mx-2 text-sm text-base-black font-semibold'}>{' to '}</p>
+                        <DateTimeListBox
+                            dataSource={dateTimeDateSource}
+                            selected={'08:00'}
+                            onChangeValue={(value) => {
+
+                            }}
+                        />
+                    </div>
+                </td>
             </tr>
         )
     }
     return (
-        <div className={'w-full h-screen bg-yellow-500'}>
+        <div className={'w-full h-screen bg-white'}>
             <div className={'m-4 border-b'}>
                 <h1>Settings</h1>
             </div>
@@ -59,8 +100,13 @@ export default function Settings() {
                     <thead>
                         <tr>
                             <th></th>
-                            <th>AM</th>
-                            <th>PM</th>
+                            <th>
+                                <p className={'font-mono text-md text-base-black font-semibold py-2'}>AM</p>
+                            </th>
+                            <th></th>
+                            <th>
+                                <p className={'font-mono text-md text-base-black font-semibold py-2'}>PM</p>
+                            </th>
                         </tr>
                     </thead>
                     <tfoot>
@@ -70,7 +116,7 @@ export default function Settings() {
                         </tr>
                     </tfoot>
                     <tbody>
-                    {renderWorkingHourItem()}
+                        {renderWorkingHourItem()}
                         <tr>
                             <td>February</td>
                             <td>$80</td>
